8d821dc8d6472ee288c50dd773f05d389db53f72
Justin Caratzas <bigjust@lambdaphil.es>
2017-05-28 20:22:12 -0400

    add leaderboard

--- helga_oral_history.py
+++ helga_oral_history.py
@@ -1,7 +1,13 @@
 import datetime
+import random
 
+from bson.son import SON
+
+from helga import log
 from helga.db import db
-from helga.plugins import preprocessor
+from helga.plugins import preprocessor, command
+
+logger = log.getLogger(__name__)
 
 
 def redact(message):
@@ -12,16 +18,82 @@ def redact(message):
 
     return message
 
+def obfuscate_nick(nick):
+    """
+    Replaces a random character with a '_' to prevent highlighting.
+    """
+
+    index = random.randrange(len(nick))
+
+    return nick[:index] + '_' + nick[index+1:]
+
 @preprocessor
-def oral_history(client, channel, nick, message):
+@command('oral', help='oral history leaderboard')
+def oral_history(client, channel, nick, message, *args):
+    """
 
-    redacted_message = redact(message)
+    Dual Preprocessor/Command.
 
-    db.logger.insert({
-        'channel': channel,
-        'nick': nick,
-        'message': redacted_message,
-        'timestamp': datetime.datetime.utcnow(),
-    })
+    Command handles search/stats, while the preprocessor handles
+    logging statements.
 
-    return (channel, nick, redacted_message)
+    Preprocessor:
+
+    Redacts message and stores in mongo w/ nick, channel, message and
+    timestamp.
+
+    Command:
+    Returns top 5 prolific irc'ers
+
+    usage: ,oral [day|week|search] [<search_pattern>]
+    """
+
+    if len(args) == 0:
+
+        redacted_message = redact(message)
+
+        db.logger.insert({
+            'channel': channel,
+            'nick': nick,
+            'message': redacted_message,
+            'timestamp': datetime.datetime.utcnow(),
+        })
+
+        return (channel, nick, redacted_message)
+
+    if len(args) == 2:
+
+        start_date = datetime.datetime.utcnow()
+        #return args[1]
+
+        logger.debug('args: {}'.format(args))
+
+        if args[1][0] == 'day':
+            start_date -= datetime.timedelta(days=1)
+
+        elif args[1][0] == 'week':
+            start_date -= datetime.timedelta(days=7)
+
+        logger.debug('start_date: {}'.format(start_date))
+
+        pipeline = [
+            { "$match": {
+                "channel": channel,
+                "timestamp": {"$gte": start_date },
+            }},
+            { "$group": {"_id": "$nick", "count": {"$sum": 1}}},
+            { "$sort": SON([("count", -1), ("_id", -1)])}
+        ]
+
+        top_5 = [nick for nick in db.logger.aggregate(pipeline)][:5]
+
+        logger.debug('top_5: {}'.format(top_5))
+
+        place = 0
+        for nick in top_5:
+            client.msg(channel, '{}. {} [{}]'.format(
+                place + 1,
+                obfuscate_nick(top_5[place]['_id']),
+                top_5[place]['count'],
+            ))
+            place += 1
